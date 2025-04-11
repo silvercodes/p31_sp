9e920259d6b85c41979a6573f22b8be058167e33
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-04-11 18:14:12 +0300

    add: thread

---
+++ 03_thread/03_thread/Program.cs
@@ -0,0 +1,102 @@
+﻿
+//Thread t = new Thread(ShowPlus);
+//t.Start();
+
+//Console.WriteLine(t.IsAlive);
+
+//for (int i = 0; i < 1000; ++i)
+//    Console.Write('0');
+
+//void ShowPlus()
+//{
+//    for(int i = 0; i < 1000; ++i)
+//        Console.Write('+');
+//}
+
+
+
+
+
+//void Run()
+//{
+//    for(int i = 0; i < 5; ++i)
+//        Console.Write(0);
+//}
+
+//new Thread(Run).Start();
+//Run();
+
+
+
+
+// =====ПотокоНЕБЕЗОПАСНЫЙ код
+
+//bool done = false;
+
+//void Run()
+//{
+//    if (! done)
+//    {
+//        done = true;
+//        Console.WriteLine("*");
+//        Console.WriteLine("DONE");
+//    }
+//}
+
+//new Thread(Run).Start();
+//Run();
+
+
+
+
+// =====ПотокоНБЕЗОПАСНЫЙ код
+
+//bool done = false;
+//object locker = new object();
+
+//void Run()
+//{
+//    lock(locker)
+//    {
+//        if (!done)
+//        {
+//            done = true;
+//            Console.WriteLine("*");
+//            Console.WriteLine("DONE");
+//        }
+//    }
+    
+//}
+
+//new Thread(Run).Start();
+//new Thread(Run).Start();
+//new Thread(Run).Start();
+//new Thread(Run).Start();
+//Run();
+
+
+
+
+
+
+//void Run()
+//{
+//    while(true)
+//    {
+
+//    }
+//    for (int i = 0; i < 1000; ++i)
+//    {
+//        Console.Write('*');
+//        Thread.Sleep(1);
+//        // Thread.Sleep(TimeSpan.FromSeconds(1));
+//    }
+//    Console.WriteLine();
+//}
+
+//Thread t = new Thread(Run);
+//t.Start();
+
+//t.Join(500);
+
+//Console.WriteLine("Main finished");
