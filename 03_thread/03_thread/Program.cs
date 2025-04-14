6af1c917cb4686eda79832f6d20d03d94a7adddb
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-04-14 20:01:21 +0300

    TPL

--- 03_thread/03_thread/Program.cs
+++ 03_thread/03_thread/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+#region Intro
+
 //Thread t = new Thread(ShowPlus);
 //t.Start();
 
@@ -65,7 +67,7 @@
 //            Console.WriteLine("DONE");
 //        }
 //    }
-    
+
 //}
 
 //new Thread(Run).Start();
@@ -100,3 +102,203 @@
 //t.Join(500);
 
 //Console.WriteLine("Main finished");
+
+#endregion
+
+
+#region Create / Start
+
+// ======= Простые методы =======
+
+//void Run()
+//{
+//    Console.WriteLine("hello");
+//}
+
+//Thread t1 = new Thread(Run);
+//Thread t2 = new Thread(new ThreadStart(Run));
+
+//t2.Start();
+
+
+
+
+//Thread t = new Thread(() => Console.WriteLine("Vasia"));
+//t.Start();
+
+
+//string name = "Vasia";
+//Thread t = new Thread(() => Console.WriteLine(name));
+//t.Start();
+
+
+//void Calc(int a, int b)
+//{
+//    Console.WriteLine($"a + b = {a + b}");
+//}
+
+//int a = 5;
+//int b = 9;
+
+//Thread t = new Thread(() => Calc(a, b));
+
+
+
+//void Render(object? val)
+//{
+//    Console.WriteLine(val.ToString());
+//}
+
+//Thread t = new Thread(Render);
+//t.Start("Petya");
+
+
+
+//void RenderColoredMessage(string message, ConsoleColor color)
+//{
+//    Console.ForegroundColor = color;
+//    Console.WriteLine(message);
+//    Console.ResetColor();
+//}
+
+//string msg = "Hello";
+//ConsoleColor color = ConsoleColor.Red;
+//Thread t = new Thread(() => RenderColoredMessage(msg, color));
+//t.Start();
+
+
+
+
+// ======= "Подводный камень" с лямбдой =======
+
+// :-(((
+//for (int i = 0; i < 10; ++i)
+//    new Thread(() => Console.WriteLine(i)).Start();
+
+// :-))
+//for (int i = 0; i < 10; ++i)
+//{
+//    int x = i;
+//    new Thread(() => Console.WriteLine(x)).Start();
+//}
+
+
+
+//int i;
+//List<Thread> threads = new List<Thread>();
+//for (i = 0; i < 10; ++i)
+//    threads.Add(new Thread(() => Console.WriteLine(i)));
+
+//threads.ForEach(t => t.Start());
+
+
+
+
+// ======= Отладка =======
+
+//void Run()
+//{
+//    Console.WriteLine($"Message from {Thread.CurrentThread.Name}");
+//    Console.WriteLine();
+//}
+
+//Thread.CurrentThread.Name = "main";
+//Thread t = new Thread(Run)
+//{
+//    Name = "worker"
+//};
+//t.Start();
+//Run();
+
+
+
+
+//Thread t = new Thread(() => Console.WriteLine("hohoho"));
+
+//if (args.Length > 0)
+//    t.IsBackground = true;
+
+//t.Start();
+
+#endregion
+
+
+#region try / catch
+
+// :-(((
+//void Run()
+//{
+//    throw new Exception("Test Exception");
+//}
+
+//try
+//{
+//    new Thread(Run).Start();
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"ERROR: {ex.Message}");
+//}
+
+// :-)))
+//void Run()
+//{
+
+//    try
+//    {
+//        throw new Exception("Test Exception");
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine($"ERROR: {ex.Message}");
+//    }
+//}
+
+//new Thread(Run).Start();
+
+#endregion
+
+
+#region Tread pooling, TPL (Task Parallel Library)
+
+//// Task     Parallel        Task<T>      ValueTask<> ......
+
+//void Run()
+//{
+//    Console.WriteLine("Vasia");
+//}
+
+//Task task = Task.Factory.StartNew(Run);
+
+////
+////
+
+//task.Wait();
+
+//await task;
+
+
+
+
+
+//using System.Net;
+
+//string DownloadPage(string ulr)
+//{
+//    WebClient client = new WebClient();
+//    return client.DownloadString(ulr);
+//}
+
+//// Console.WriteLine(DownloadPage(@"https://google.com"));
+
+//string url = @"https://google.com";
+
+//Task<string> task = Task.Factory.StartNew(() => DownloadPage(url));
+////
+////
+//string content = task.Result;                                       // BLOCKING!!!
+//Console.WriteLine(content);
+
+
+
+#endregion
---
+++ 04_synchronisation/04_synchronisation/Program.cs
@@ -0,0 +1,12 @@
+﻿// ==== Инструменты синхронизации ====
+
+// 1. Простые методы блокировки (Thread.Sleep(...), Thread.Join(...), Task.Wait(...) .......)
+
+// 2. Контроль критической секции (lock, Monitor{20нс}, Mutex{1000нс}, SpinLock, Semaphore{1000нс}, SemaphoreSlim .....)
+
+// 3. Инструменты сигнализации (Monitor.Pulse(), .PulseAll(), .Wait(), AutoResetEvent, ManualResetEvent, CountdownResetEvent....)
+
+// 4. Неблокирующие инструменты (MemoryBarrier, Interlocked, Thread.VolitileRead.....)
+
+
+
